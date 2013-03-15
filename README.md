4d60e7c0b13e634f5a0019a276b16dc3e85b48aa
Neil Carpenter <neilanthonycarpenter@gmail.com>
2013-03-15 14:10:46 +0000

    Add files

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-simple-social-sharing
-=====================
+# Super simple social sharings with jQueries
 
-Use Twitter and Facebook sharing as modals please, thanks.
+Just for Twitter / Facebook at the mo, opens up proper share dialog, none of that cheap `sharer.php?url=whatever` crap.
+
+## Usage
+
+Mark up yo links as per:
+
+	<a data-[TYPE]="[SITE]" data-[CONTENT]="CONTENT" href="[CANONICAL SHARE URL]">Share on the [SITE]</a>
+
+So for a real world example:
+
+	<a data-share="facebook" data-title="Gizoogle that shiznit" data-link="http://gizoogle.net" href="https://www.facebook.com/sharer/sharer.php?u=gizoogle.net">Tell your Facebook friends! They'll love it!!</a>
+	
+
+And then the jQuery:
+
+	$( parentContainer ).socialSharers();
+
+It's using parent container for selector to allow event delegation, I'm presuming you would always have twitter and facebook buttons. It doesn't matter if you only have one, still just use parent container, or just `document`, whatever.
+
+And for some customisation extras:
+
+	$( parentContainer ).socialSharers({
+		twitter: {
+			handle: 'neilcarpenter' // will add 'via @neilcarpenter' on tweets
+		},
+		facebook: {
+			appID: 144136645754967 // will load FB SDK asynchronously and add 'via [FB APP NAME]'
+		}
+	});
+
+All content / events are handled through data-* attributes on the links themselves, so your options are:
+
+- data-share => 'facebook' or 'twitter'
+- data-title => Title of content to share (fb), or share text (tw). *Defaults to document `<title>`*
+- data-link => Link to share. *Defaults to current `window.location.href`*
+- data-image => Picture to share (fb only)
+- data-description => Share text (fb only)
+
+**Check `demo.html` for working example.**
---
+++ jquery.socialSharers.js
@@ -0,0 +1,143 @@
+/*
+ *  Project: jQuery simple social sharing
+ *  Description: Tweet / post to FB using proper modals
+ *  Author: Neil Carpenter
+ */
+
+;(function ($, window, document, undefined) {
+
+    var pluginName = 'socialSharers';
+    var defaults = {
+        twitter: {
+            handle: null
+        },
+        facebook: {
+            appID: null
+        }
+    };
+
+    function SocialSharers(element, options) {
+
+        this.element = $( element );
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+
+    }
+
+    SocialSharers.prototype = {
+        init: function () {
+
+            this.loadScripts();
+            this.events();
+
+        },
+        loadScripts: function () {
+
+            // asychroniously loads external scripts
+            // From https://gist.github.com/necolas/1025811
+            var js,
+                fjs = document.getElementsByTagName('script')[0],
+                add = function(url, id) {
+                    if (document.getElementById(id)) {return;}
+                    js = document.createElement('script');
+                    js.src = url;
+                    id && (js.id = id);
+                    fjs.parentNode.insertBefore(js, fjs);
+                };
+
+            // Facebook SDK
+            if ( this.options.facebook.appID && this.element.find('[data-share="facebook"]').length ) add('//connect.facebook.net/en_US/all.js', 'facebook-jssdk');
+
+        },
+        facebook: function( $el ) {
+
+            var link = $el.data('link') || window.location.href;
+            var obj, url, width, height, leftPosition, topPosition;
+
+            if ( this.options.facebook.appID ) {
+
+                obj = {
+                    method: 'feed',
+                    link: link,
+                    picture: $el.data('image') || '',
+                    name: $el.data('title') || document.title,
+                    description: $el.data('description') || ''
+                };
+
+                FB.ui(obj);
+
+            } else {
+
+                width = 640;
+                height = 320;
+                leftPosition = ( screen.width / 2 ) - ( width / 2 );
+                topPosition = ( screen.height / 2 ) - ( height / 2 );
+
+                url = 'https://www.facebook.com/sharer/sharer.php?u=';
+                url += encodeURIComponent( link );
+                window.open(url, 'fbshare', 'width='+width+', height='+height+', top='+topPosition+', left='+leftPosition+', menubar=no, status=no, toolbar=no, ');
+
+            }
+
+        },
+        twitter: function( $el ) {
+
+            var url;
+            var link = ( $el.data('link') ) ? $el.data('link') : window.location.href;
+            var title = ( $el.data('title') ) ? $el.data('title') : document.title;
+            var via = ( this.options.twitter.handle ) ? '&via=' + this.options.twitter.handle : '';
+
+            var width = 550;
+            var height = 450;
+            var leftPosition = ( screen.width / 2 ) - ( width / 2 );
+            var topPosition = ( screen.height / 2 ) - ( height / 2 );
+
+            url = 'https://twitter.com/share?url=' + encodeURIComponent( link ) +
+                '&text=' + encodeURIComponent( title ) + via;
+
+            window.open(url, 'twshare', 'width='+width+', height='+height+', top='+topPosition+', left='+leftPosition+', menubar=no, status=no, toolbar=no, ');
+
+        },
+        events: function() {
+
+            var self = this;
+
+            this.element.on('click', 'a', function( e ) {
+
+                e.preventDefault();
+
+                var $el = $(e.target);
+
+                switch ( $el.data('share') ) {
+                    case 'facebook': self.facebook( $el ); break;
+                    case 'twitter': self.twitter( $el ); break;
+                }
+
+                $el.blur();
+
+            });
+
+            if ( this.options.facebook.appID ) {
+
+                window.fbAsyncInit = function() {
+                    FB.init({appId: self.options.facebook.appID, status: false});
+                };
+
+            }
+
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new SocialSharers(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
